567046ca52fa35506de420ace266f94bc1c8096a
Dharmesh Mishra <dharmeshmishra@Dharmeshs-MacBook-Air.local>
2025-09-18 02:54:09 +0530

    fix: make root page dynamic to fix build error

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,10 @@ import {recentSessions} from "@/constants";
 import {getAllCompanions, getRecentSessions} from "@/lib/actions/companion.action";
 import {getSubjectColor} from "@/lib/utils";
 
+
+export const dynamic = 'force-dynamic';
+
+
 const Page = async () => {
 
     const companions = await getAllCompanions({limit:3});
